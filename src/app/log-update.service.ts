af705492e92e53a3536288b74efbe8169654a59a
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-12-16 14:30:01 +0100

    Add ng13 styled update logger

--- src/app/log-update.service.ts
+++ src/app/log-update.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,26 @@ export class LogUpdateService {
 
   constructor(updates: SwUpdate) {
     
-    updates.available.subscribe(event => {
+    // updates.available.subscribe(event => {
+    //   console.log('current version is', event.current);
+    //   console.log('available version is', event.available);
+    // })
+
+    // updates.activated.subscribe(event => {
+    //   console.log('old version was', event.previous);
+    //   console.log('new version is', event.current);
+    // })
+
+    updates.versionUpdates.pipe(
+      filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+      map(evt => ({
+        type: 'UPDATE_AVAILABLE',
+        current: evt.currentVersion,
+        available: evt.latestVersion,
+      }))
+    ).subscribe(event => {
       console.log('current version is', event.current);
       console.log('available version is', event.available);
-    })
-
-    updates.activated.subscribe(event => {
-      console.log('old version was', event.previous);
-      console.log('new version is', event.current);
-    })
+    });
   }
 }
