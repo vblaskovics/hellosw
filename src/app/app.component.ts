404d66c0f1d2339c9d8cef2f97aaa42247460b52
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-12-16 14:01:55 +0100

    Add SwUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'hellosw';
+  updateCheckText = '';
+
+  constructor(private update: SwUpdate) {}
+
+  updateCheck(): void {
+    this.update //
+      .checkForUpdate()
+      .then(() => this.updateCheckText = 'resolved')
+      .catch(err => this.updateCheckText = `rejected: ${err.message}`);
+  }
 }
